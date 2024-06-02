bb17cbae1b3af8c89f084bdd27ad1b906ce6f958
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-06-02 17:11:50 +0000

    handling array object and function

---
+++ test/basic.js
@@ -0,0 +1,14 @@
+let Array = [1,2,3,"rhm","mustafa"]
+
+let Obj = {
+    name: "rahamtullah sheikh",
+    age: 20,
+}
+
+const MyFunction = function(){
+    console.log("hello world")
+}
+
+console.log(Array);
+console.log(Obj);
+console.log(MyFunction());
