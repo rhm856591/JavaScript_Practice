2626a53b015acfec58dd61164bb3c5f6c4498d1e
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-06-02 07:55:18 +0000

    first program

---
+++ test/firstProgram.js
@@ -0,0 +1,11 @@
+console.log("hello, world");
+
+let number = 99
+
+// let string = Number(number);
+let string = "abcd"
+
+console.log(string);
+console.log(number);
+
+console.table([{number, string}])
