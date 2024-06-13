c0a60f268de385718f80f679628d83187432271c
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-06-13 11:36:54 +0000

    object arrow and iife

--- Basic_02/Object_02.js
+++ Basic_02/Object_02.js
@@ -29,7 +29,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+console.log(obj3);
 
 
 const users = [
